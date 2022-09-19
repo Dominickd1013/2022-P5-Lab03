7c0c80064efa8ffe57a9ebfb33713b670c27b6a1
Dominick <domenico.damato@vtsd.com>
2022-09-19 13:51:48 -0400

    This is my interpretation of how to do the assignment

---
+++ Lab03st80Point.java
@@ -0,0 +1,35 @@
+public class Lab03st80Point
+{
+
+    public static void main(String[] args)
+
+    {
+        int num = 10000123;
+        System.out.println("Lab03, 100 Point Version\n");
+        System.out.print("Starting milli-seconds:");
+        System.out.print(" ");
+        System.out.print(num);
+        System.out.println();
+        System.out.print("Hours:");
+        System.out.print("                  ");
+        System.out.print(num/3600000);
+        System.out.println();
+        System.out.print("Minutes:");
+        System.out.print("                ");
+        System.out.print(num/215000);
+        System.out.println();
+        System.out.print("Seconds:");
+        System.out.print("                ");
+        System.out.print(num/245000);
+        System.out.println();
+        System.out.print("Milliseconds:");
+        System.out.print("           ");
+        System.out.print(num/81010);
+        System.out.println();
+
+
+
+
+    }
+
+}
